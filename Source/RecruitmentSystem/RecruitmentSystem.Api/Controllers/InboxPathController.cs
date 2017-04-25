2ec1c6d8bb0b2548747ec02e6191c25c03c3867a
booma1234 <Prabath.Boomawalage@gmail.com>
2017-04-25 14:57:04 +0530

    inbox reader

---
+++ Source/RecruitmentSystem/RecruitmentSystem.Api/Controllers/InboxPathController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.IO;
+using System.Net.Http.Headers;
+
+
+namespace RecruitmentSystem.API.Controllers
+{
+    public class InboxPathController : ApiController
+    {
+
+        public HttpResponseMessage GetFile(string id)
+        {
+            if (String.IsNullOrEmpty(id))
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+
+            string fileName = "prabath@creativesoftware.com - SRS.pdf";
+            string localFilePath= "D:\\CV-Attachment";
+            
+
+            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = new StreamContent(new FileStream(localFilePath, FileMode.Open, FileAccess.Read));
+            response.Content.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment");
+            response.Content.Headers.ContentDisposition.FileName = fileName;
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/pdf");
+
+            return response;
+        }
+    }
+}
---
+++ Source/RecruitmentSystem/RecruitmentSystem.Data/Inbox.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecruitmentSystem.Data
+{
+  public  class Inbox
+    {
+        public int InboxID { get; set; }
+
+        public String EmailName { get; set; }
+
+        public String EmailSubject { get; set; }
+
+        public String EmailBodyText { get; set; }
+
+        public String CvPath { get; set; }
+
+        public DateTime ArrivalTime { get; set; }
+
+
+
+    }
+}
